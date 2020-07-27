34f254084dc57c00b31334ef72bdb1c1a187fb81
luoxin971 <luoxin9712@163.com>
2020-07-27 18:26:46 +0800

    add hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: luoxin971
+# @Date:   2020-07-27 18:25:18
+# @Last Modified by:   luoxin971
+# @Last Modified time: 2020-07-27 18:26:17
+
+def main():
+	print("Hello wrold")
+	
+
+if __name__ == '__main__':
+	main()
