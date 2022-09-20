6081aa69b4ea8fc6689411eb59a04a62792a4b43
luoxin971 <47984356+luoxin971@users.noreply.github.com>
2022-09-20 23:59:39 +0800

    Update sleepcycle.js

--- sleepcycle.js
+++ sleepcycle.js
@@ -5,17 +5,17 @@ Sleep cycle 会员
 QuantumultX:
 
 [rewrite_local]
-^https:\/\/ch\.sleepcycle\.com\/api\/v1\/subscription\/get url script-response-body abc.js
+^https:\/\/ch\.sleepcycle\.com\/api\/v1\/subscription\/get url script-response-body https://raw.githubusercontent.com/luoxin971/Study-notes/master/sleepcycle.js
 
 [mitm]
-hostname = mp.weixin.qq.com
+hostname = *.sleepcycle.com
 
 **************************/
 
 var obj = JSON.parse($response.body);
-obj.subscription.expire_date = 1734221586
-abc = obj.subscription.features_rich
-for (x in abc) {
+obj.subscription.expire_date = 1734221588
+// abc = obj.subscription.features_rich
+for (x of obj.subscription.features_rich) {
     x.expire_date = 1734221586
 }
 $done({body: JSON.stringify(obj)}); 
