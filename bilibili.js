52c92c490c13c732a403c96f106efa215675e716
luoxin971 <47984356+luoxin971@users.noreply.github.com>
2022-09-22 01:55:20 +0800

    Update bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -13,11 +13,11 @@ hostname = app.bilibili.com
 **************************/
 
 var obj = JSON.parse($response.body);
-list = obj.data.items
-console.log("originLength: ", list.length)
-filter = list.filter(x => x.ad_info == undefined)
-console.log("filterLength: ", filter.length)
-ad = list.filter(x => !filter.includes(x))
+var list = obj.data.items
+console.log(list.length)
+var filter = list.filter(x => x.ad_info == undefined)
+console.log(filter.length)
+var ad = list.filter(x => !filter.includes(x))
 console.log(ad)
 obj.data.items = filter
 $done({body: JSON.stringify(obj)}); 
