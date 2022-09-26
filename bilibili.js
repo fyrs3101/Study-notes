119da7f4fb85f9e5a9b41b136c346a4f09418ff3
luoxin971 <47984356+luoxin971@users.noreply.github.com>
2022-09-26 19:25:04 +0800

    Update bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -6,16 +6,21 @@ QuantumultX:
 
 [rewrite_local]
 ^https:\/\/app\.bilibili\.com\/x\/v2\/feed\/index url script-response-body https://raw.githubusercontent.com/luoxin971/Study-notes/master/bilibili.js
+^https:\/\/app\.biliapi\.net\/x\/v2\/feed\/index url script-response-body https://raw.githubusercontent.com/luoxin971/Study-notes/master/bilibili.js
 
 [mitm]
 hostname = app.bilibili.com
+hostname = app.biliapi.net
 
 **************************/
 var obj = JSON.parse($response.body);
 console.log("filter begin")
 var list = obj.data.items
 console.log(list.length)
-var filter = list.filter(x => x.card_goto == "av" || x.card_goto == "banner")
+var filter = list.filter(x => {
+    var str = JSON.stringify(x)
+    return !str.includes("推广")
+})
 console.log(filter.length)
 var ad = list.filter(x => !filter.includes(x))
 console.log(JSON.stringify(ad))
