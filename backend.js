45e085414e7fbdf727e7c935ff786a88c1f1685c
luoxin971 <47984356+luoxin971@users.noreply.github.com>
2023-01-30 17:32:47 +0800

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,6 +1,5 @@
 const headers = {
     "X-GitHub-Api-Version": "2022-11-28",
-    'Authorization': 'Bearer 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',
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
 };
 const url = 'https://api.github.com/users/luoxin971/repos';
@@ -12,8 +11,8 @@ const myRequest = {
 
 $task.fetch(myRequest).then(response => {
     // response.statusCode, response.headers, response.body
-    console.log(response.body);
-    $notify("Title", "Subtitle", response.body); // Success!
+    console.log(response.body.length);
+    $notify("Title", "Subtitle", response.body.length); // Success!
     $done();
 }, reason => {
     // reason.error
