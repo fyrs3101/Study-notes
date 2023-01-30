4889d15bbc01a86c66ceadbd4ac5913d3db38a88
luoxin971 <47984356+luoxin971@users.noreply.github.com>
2023-01-30 15:46:18 +0800

    Create backend.js

---
+++ backend.js
@@ -0,0 +1,39 @@
+const https = require('https');
+
+h = {
+    "X-GitHub-Api-Version": "2022-11-28",
+    'Authorization': 'Bearer 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',
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+}
+const resContent = "";
+https.get('https://api.github.com/users/luoxin971/repos', { headers: h }, (response) => {
+    let todo = '';
+
+    // called when a data chunk is received.
+    response.on('data', (chunk) => {
+        todo += chunk;
+        resContent += chunk;
+    });
+
+    // called when the complete response is received.
+    response.on('end', () => {
+        let a = JSON.parse(todo);
+        a.map(element => {
+            console.log(element.url + " " + element.name);
+        });
+    });
+
+}).on("error", (error) => {
+    console.log("Error: " + error.message);
+});
+
+const myStatus = "HTTP/1.1 200 OK";
+const myHeaders = { "Connection": "Close" };
+
+const myResponse = {
+    status: myStatus,
+    headers: myHeaders,
+    body: resContent
+};
+
+$done(myResponse)
