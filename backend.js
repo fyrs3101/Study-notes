c9d14bbeb32845456bf5582bf934d97f4ebe135a
luoxin971 <47984356+luoxin971@users.noreply.github.com>
2023-01-30 16:09:08 +0800

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,28 +1,23 @@
-h = {
+const headers = {
     "X-GitHub-Api-Version": "2022-11-28",
     'Authorization': 'Bearer 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',
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
 }
+cosnt url = 'https://api.github.com/users/luoxin971/repos';
+cosnt method = 'GET';
+const myRequest = {
+    url: url,
+    method: method, // Optional, default GET.
+    headers: headers
+}
 let resContent = "";
-$task.get('https://api.github.com/users/luoxin971/repos', { headers: h }, (response) => {
-    let todo = '';
+$task.fetch(myRequest).then(response => {
+    resContent = response.body;
+    $notify("Success", "Subtitle", response.body); // Success!
 
-    // called when a data chunk is received.
-    response.on('data', (chunk) => {
-        todo += chunk;
-        resContent += chunk;
-    });
-
-    // called when the complete response is received.
-    response.on('end', () => {
-        let a = JSON.parse(todo);
-        a.map(element => {
-            console.log(element.url + " " + element.name);
-        });
-    });
-
-}).on("error", (error) => {
-    console.log("Error: " + error.message);
+}, reason => {
+    $notify("Error", "Subtitle", reason.error); // Error!
+    console.log("Error: " + reason.error);
 });
 
 const myStatus = "HTTP/1.1 200 OK";
